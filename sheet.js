ecea9a85ee64a7f7f935bf7a03630d82db349620
Alina <alinadanila1643@gmail.com>
2023-11-17 11:47:57 +0200

    contact working

---
+++ sheet.js
@@ -0,0 +1,10 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyg4duQRjxsI8MjFm75guTSgYMIqfGizm8TYBBDb_ZxAociWxuJZkSOchPlhsmjsBSXqw/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+})
